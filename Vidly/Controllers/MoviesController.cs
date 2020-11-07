2fa31ad13d58cc2b44785cc135e36b3363834b44
Khaled El Hamzi <khaled.elhamzi.syne19lin@tucsweden.se>
2020-11-07 16:59:04 +0100

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Films.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace Films.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shark!"};
+            return View(movie);
+           
+        } 
+    }
+}
