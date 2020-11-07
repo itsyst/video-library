d3c3df217b34b91a3e8c3541e58dc2861dae508d
Khaled El Hamzi <khaled.elhamzi.syne19lin@tucsweden.se>
2020-11-07 17:30:24 +0100

    Query objects

--- Vidly/Data/ApplicationDbContext.cs
+++ Vidly/Data/ApplicationDbContext.cs
@@ -1,19 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Vidly.Models;
+
 
 namespace Vidly.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public DbSet<Customer> Customers { get; set; }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            SeedDatabase(modelBuilder);
+
+            ConfigureCustomer(modelBuilder);
+ 
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private static void SeedDatabase(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .HasData(
+                          new Customer { Id = 1, Name = "John Smith", IsSubscribedToNewsLetter = false, MembershipTypeId = 1 },
+                          new Customer { Id = 2, Name = "Mary William", IsSubscribedToNewsLetter = true, MembershipTypeId = 2 }
+                );
+
+            modelBuilder.Entity<MembershipType>()
+                .HasData(
+                    new MembershipType { Id = 1, SignUpFee = 0, DurationInMonths = 0, DiscountRate = 0 },
+                    new MembershipType { Id = 2, SignUpFee = 30, DurationInMonths = 1, DiscountRate = 10 },
+                    new MembershipType { Id = 3, SignUpFee = 90, DurationInMonths = 3, DiscountRate = 15 },
+                    new MembershipType { Id = 4, SignUpFee = 300, DurationInMonths = 12, DiscountRate = 20 }
+                );
+        }
+
+        private static void ConfigureCustomer(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Customer>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+        }
+
     }
 }
--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Vidly.Models
 {
+    [Table("MembershipTypes")]
     public class MembershipType
     {
         public byte Id { get; set; }
