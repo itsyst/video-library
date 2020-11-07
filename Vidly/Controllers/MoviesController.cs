be52ca7e2fa52aa092846ddc494eb19b54c4b14d
Khaled El Hamzi <khaled.elhamzi.syne19lin@tucsweden.se>
2020-11-07 17:06:34 +0100

    Added action parameters(input)

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -20,5 +20,24 @@ namespace Vidly.Controllers
             return RedirectToAction("Index", "Home", new {page=1,sortBy="name"});
         }
 
+        public IActionResult Edit(int id)
+        {
+            return Content("id=" +id);
+        }
+
+        //movies
+        public IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public IActionResult ByReleaseDate(int year, byte month)
+        {
+            return Content(year + "/" + month);
+        }
     }
 }
