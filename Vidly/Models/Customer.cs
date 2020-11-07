791caf37b167ecbcc2f4110dd2c2f6a905e56b45
Khaled El Hamzi <khaled.elhamzi.syne19lin@tucsweden.se>
2020-11-07 17:34:35 +0100

    Form labels

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -14,6 +14,8 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
 
+
+        [Display(Name = "Date of birth")]
         public DateTime? Birthdate { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType{ get; set; }
