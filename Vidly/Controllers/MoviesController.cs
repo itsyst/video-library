2eee22dac9370530194f9e7058b8a8fc542a9a3d
Khaled El Hamzi <khaled.elhamzi.syne19lin@tucsweden.se>
2020-11-07 17:09:29 +0100

    Passing Data to Views

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,35 +10,16 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        //GET: Movies/Random
         public IActionResult Random()
         {
             var movie = new Movie() {Name = "Shark!"};
-            // return View(movie);
-            // return Content("Hello World!"); // We get a plain string content in the response
-            // return NotFound();  // standard 404 error
-            // return new EmptyResult();
-            return RedirectToAction("Index", "Home", new {page=1,sortBy="name"});
+
+            // ViewData["Movie"] = movie; // view data dictionary
+            // ViewBag.Movie = movie;// instead of using viewData with magic string we use viewBag with magic property
+            return View(movie);
         }
 
-        public IActionResult Edit(int id)
-        {
-            return Content("id=" +id);
-        }
-
-        //movies
-        public IActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-
-        [Route("movies/released/{year}/{month:regex(\\d{{2}}):range(1,12)}")] // route attribute && constraint regex(\\d{4})
-        public IActionResult ByReleaseDate(int year, byte month)
-        {
-            return Content(year + "/" + month);
-        }
+       
     }
 }
